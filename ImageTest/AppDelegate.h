f25193ec5f733ed1bf99fabb8c6418ee4a529b24
Artur Mazurek <arturm@Arturs-Mac.local>
2012-08-17 12:09:58 +0100

    Initial Commit

---
+++ ImageTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageTest
+//
+//  Created by Artur Mazurek on 17/08/2012.
+//  Copyright (c) 2012 Artur Mazurek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
