4612300f1a6f7938a031e002750f55169ea4ca8c
Artur Mazurek <artur.s.mazurek@gmail.com>
2013-06-27 21:47:41 +0100

    Playing with sun position. Still when it's in proper location the posibility to hit is too small

--- Raytracer/AppDelegate.mm
+++ Raytracer/AppDelegate.mm
@@ -89,11 +89,11 @@ static const FloatType SUN_RADIUS = 6.96342e5 * 1000;
     s.addObject(std::unique_ptr<BaseObject>{new Cylinder{{RINGWORLD_EXTENDS + RIM_WALL_EXTENDS, RINGWORLD_RADIUS - 2, 0}, RINGWORLD_RADIUS - RIM_WALL_HEIGHT, RIM_WALL_EXTENDS, Cylinder::AxisAlignment::X_AXIS}});
     s.addObject(std::unique_ptr<BaseObject>{new Cylinder{{-(RINGWORLD_EXTENDS + RIM_WALL_EXTENDS), RINGWORLD_RADIUS - 2, 0}, RINGWORLD_RADIUS - RIM_WALL_HEIGHT, RIM_WALL_EXTENDS, Cylinder::AxisAlignment::X_AXIS}});
     
-//    s.addLight(std::unique_ptr<BaseLight>{new SphereLight{{0, RINGWORLD_RADIUS, 0}, SUN_RADIUS}});
+    auto light = std::unique_ptr<BaseObject>{new Sphere{{0, 15 * SUN_RADIUS, 0}, SUN_RADIUS}};
 //    auto light = std::unique_ptr<BaseObject>{new Sphere{{0, 4 * SUN_RADIUS, 0}, SUN_RADIUS}};
 //    auto light = std::unique_ptr<BaseObject>{new Sphere{{-1.5, 2, 10}, 0.5}};
-        auto light = std::unique_ptr<BaseObject>{new Sphere{{-1.5, 200, 10}, 50}};
-    light->material()->setEmmitance({20, 20, 20, 1});
+//        auto light = std::unique_ptr<BaseObject>{new Sphere{{-1.5, 20000, 10}, 5000}};
+    light->material()->setEmmitance({200, 200, 200, 1});
 //    light->emits = true;
     s.addObject(std::move(light));
 }
